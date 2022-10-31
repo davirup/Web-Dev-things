b7e428578f03b2a1f9dace4bb5762d7e1dad1651
Avirup Das <88451080+davirup@users.noreply.github.com>
2022-10-31 21:27:28 +0530

    Add files via upload

---
+++ Web-Dev-things/orbit icons/index.js
@@ -0,0 +1,7 @@
+// var item = document.querySelectorAll(".item");
+// item.forEach(function (node, index) {
+//   item[index].addEventListener("mouseover", () => {
+//     item[index].style.backgroundColor = "blue";
+//   });
+
+// });
